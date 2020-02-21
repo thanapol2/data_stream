e8c7ff304b4a6edc717ab2fc424de3da0a39367d
Tong_RulE76 <thanapol.phu@gmail.com>
2020-02-21 20:15:15 +0700

    split file

--- sim_adwin.py
+++ sim_adwin.py
@@ -3,7 +3,7 @@ import numpy as np
 from skmultiflow.drift_detection.adwin import ADWIN
 
 
-def test_sim(input_stream,file_name='result',zoom=False,zoom_start=0,zoom_end=5000,range_zoom=0):
+def test_sim(input_stream,file_name='result',zoom=False,zoom_start=0,zoom_end=5000,range_zoom=0,plot=False):
     adwin = ADWIN()
     change_point=[]
     plt.plot(input_stream)
@@ -17,28 +17,29 @@ def test_sim(input_stream,file_name='result',zoom=False,zoom_start=0,zoom_end=50
             # print('Change detected in data: ' + str(input_stream[i]) + ' - at index: ' + str(i)+'\n\n')
             f.write('Change detected in data: ' + str(input_stream[i]) + ' - at index: ' + str(i)+'\n\n')
     f.close()
-    plt.ylabel('value')
-    plt.xlabel('Time')
-    plt.savefig("images/"+file_name+"_result.png", aspect='auto', bbox_inches='tight', dpi=200)
-    plt.show(aspect='auto')
-    if(zoom):
-        if zoom_start == 0:
-            zoom_start = change_point[0]-100
-        if zoom_end == 5000:
-            if range_zoom ==0:
-                zoom_end = change_point[-1]+100
-            else:
-                zoom_end = zoom_start+range_zoom
-        xi = list(range(zoom_start, zoom_end))
-        plt.plot(xi, input_stream[zoom_start:zoom_end])
+    if(plot):
         plt.ylabel('value')
         plt.xlabel('Time')
-        plt.savefig("images/" + file_name + "_zoom.png", aspect='auto', bbox_inches='tight', dpi=200)
-        for i in change_point:
-            if(i<=zoom_end):
-                plt.axvline(i, color='r', linestyle='dashed')
-        plt.ylabel('value')
-        plt.xlabel('Time')
-        plt.savefig("images/"+file_name+"_result_zoom.png", aspect='auto', bbox_inches='tight', dpi=200)
-        plt.show()
+        plt.savefig("images/"+file_name+"_result.png", aspect='auto', bbox_inches='tight', dpi=200)
+        plt.show(aspect='auto')
+        if(zoom):
+            if zoom_start == 0:
+                zoom_start = change_point[0]-100
+            if zoom_end == 5000:
+                if range_zoom ==0:
+                    zoom_end = change_point[-1]+100
+                else:
+                    zoom_end = zoom_start+range_zoom
+            xi = list(range(zoom_start, zoom_end))
+            plt.plot(xi, input_stream[zoom_start:zoom_end])
+            plt.ylabel('value')
+            plt.xlabel('Time')
+            plt.savefig("images/" + file_name + "_zoom.png", aspect='auto', bbox_inches='tight', dpi=200)
+            for i in change_point:
+                if(i<=zoom_end):
+                    plt.axvline(i, color='r', linestyle='dashed')
+            plt.ylabel('value')
+            plt.xlabel('Time')
+            plt.savefig("images/"+file_name+"_result_zoom.png", aspect='auto', bbox_inches='tight', dpi=200)
+            plt.show()
     return change_point
--- sim_hiend.py
+++ sim_hiend.py
@@ -21,7 +21,22 @@ plt.plot(stream_data)
 plt.ylabel('value')
 plt.xlabel('Time')
 # plt.savefig("images/"+ file_name + ".png", aspect='auto', bbox_inches='tight', dpi=200)
-plt.show()
+# plt.show()
+loop = int(len(stream_data)/20000)+1
+start_point = 0
+end_point = 0
+change_points = []
+for i in range(int(loop)):
+    if(i == 0):
+        start_point = 0
+        end_point = 20000
+    else:
+        start_point = (i*20000)-1000
+        if(i==loop-1):
+            end_point = len(stream_data)
+        else:
+            end_point = (i*20000)+20000
+    change_points.append(sim.test_sim(stream_data[start_point:end_point]))
 
-change_point = sim.test_sim(stream_data, zoom=False,range_zoom=1000)
+# change_point = sim.test_sim(stream_data, zoom=False,range_zoom=1000)
 print(change_point)
