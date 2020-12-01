80daddba70238baffa6f95240e513dff09e1d6a1
Tong_RulE76 <thanapol.phu@gmail.com>
2020-12-01 18:38:51 +0700

    update kalman

--- kalman_playground/kalman_method.py
+++ kalman_playground/kalman_method.py
@@ -2,18 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def update(prior_mean,prior_variance, measurement_mean,measurement_variance):
-    x,P = prior        # mean and variance of prior
-    z,R = measurement  # mean and variance of measurement
+    x = prior_mean
+    P = prior_variance        # mean and variance of prior
+    z= measurement_mean
+    R = measurement_variance# mean and variance of measurement
     K = P/(P+R)  # Kalman gain
     x = x+K*(z-x)  # State update
     P = (1-K)*P  # Covariance Update
     return x, P
 
-def predict(posterior, movement):
-    x, P = posterior  # mean and variance of posterior
-    dx, Q = movement  # mean and variance of movement
-    x = x + dx
-    P = P + Q
+def predict(posterior_mean,posterior_variance, movement_mean,movement_variance):
+    x = posterior_mean
+    P = posterior_variance  # mean and variance of posterior
+    dx = movement_mean
+    Q = movement_variance  # mean and variance of movement
+    x = x + dx  #State Extrapolation
+    P = P + Q  #Covariance Extrapolation
     return x, P
 
 def plot_filter(xs, ys=None, dt=None, c='C0', label='Filter', var=None, **kwargs):
--- kalman_playground/kalman_play.py
+++ kalman_playground/kalman_play.py
@@ -6,18 +6,27 @@ types = ["poisson"]
 patterns = ["sq"]
 LS = [1,5]
 IS = [100]
+process_var = 1
+process_mean = 0
 for type in types:
     for pattern in patterns:
         for L in LS:
             for I in IS:
-                data = plot_data(path='D:\\git_project\\data stream\\dataset', type=type, pattern=pattern, len=L,
+                data = plot_data(path='C:\\Users\\karnk\\git\\data_stream\\dataset', type=type, pattern=pattern, len=L,
                                  interval=I)
                 data.load_data_fromfile()
                 window = sliding_win(windows_size)
                 for i in range(data.get_file_lenght()):
                     file_name = data.get_file_name(i)
                     measurements = data.get_dataset_test(i)
+                    x_mean = 0
+                    x_variance = 400
                     for measurement in measurements:
-                        mean_cur = window.get_mean()
-                        variance_cur = window.get_variance()
-                        kalman.predict()
+                        prior_mean = window.get_mean()
+                        prior_variance = window.get_variance()
+                        window.append(measurement)
+                        measurement_mean = window.get_mean()
+                        measurement_variance = window.get_variance()
+                        prior_mean, prior_variance = kalman.predict(x_mean,x_variance,process_mean,process_var)
+                        x_mean, x_variance = kalman.update(prior_mean,prior_variance,measurement_mean,measurement_variance)
+                        print ('a')
